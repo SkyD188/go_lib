311ddbf5cdfcceabf6ce5cfefdb6637686c1972a
代泓宇 <daihy@dustess.com>
2020-08-07 13:49:40 +0800

    '修改配置'

--- config/config.go
+++ config/config.go
@@ -4,10 +4,9 @@ import (
 	"github.com/spf13/viper"
 )
 
-func InitConfig(confType, confPath string, conf interface{}) {
+func InitConfig(confPath string, conf interface{}) {
 	v := viper.New()
 
-	v.SetConfigType(confType) // 配置文件的类型
 	v.SetConfigFile(confPath) // 配置文件的路径
 
 	if err := v.ReadInConfig(); err != nil {
