1b88a9685ec531849306fa8723d98205bde2cd61
代泓宇 <daihy@dustess.com>
2020-08-07 10:58:29 +0800

    add go.sum

--- config/config.go
+++ config/config.go
@@ -4,13 +4,11 @@ import (
 	"github.com/spf13/viper"
 )
 
-func InitConfig(confName, confType, confPath string, conf interface{}) {
+func InitConfig(confType, confPath string, conf interface{}) {
 	v := viper.New()
 
-	//v.SetConfigName(confName) // 配置文件的名字
 	v.SetConfigType(confType) // 配置文件的类型
 	v.SetConfigFile(confPath) // 配置文件的路径
-	//v.AddConfigPath(confPath) // 配置文件的路径
 
 	if err := v.ReadInConfig(); err != nil {
 		panic(err)
